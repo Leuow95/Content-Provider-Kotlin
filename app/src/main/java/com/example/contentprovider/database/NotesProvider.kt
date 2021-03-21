d15a1ae3bc8119c62105a2cb5a0b4d4b18f7b889
LEONARDO <Leuow95>
2021-03-21 14:57:33 -0400

    implemented the inseert database at NotesProvider

--- app/src/main/java/com/example/contentprovider/database/NotesProvider.kt
+++ app/src/main/java/com/example/contentprovider/database/NotesProvider.kt
@@ -39,7 +39,17 @@ class NotesProvider : ContentProvider() {
     override fun getType(uri: Uri): String? = throw UnsupportedSchemeException("Uri não implementada")
 
     override fun insert(uri: Uri, values: ContentValues?): Uri? {
-        if (mUriMatcher.match(uri) == NOTES)
+        if (mUriMatcher.match(uri) == NOTES) {
+            val db: SQLiteDatabase = dbHelper.writableDatabase
+            val id = db.insert(TABLE_NOTES, null, values)
+            val insertUri = Uri.withAppendedPath(BASE_URI, id.toString())
+            db.close()
+            //Avisa o Content Provider que houve alteração
+            context?.contentResolver?.notifyChange(uri, null)
+            return insertUri
+        } else{
+            throw UnsupportedSchemeException("Uri inválida para inserção!")
+        }
     }
 
     override fun query(
